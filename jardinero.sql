4a1f673053bc3d8f7efd3db42f4997c03ab1b4b3
MaleckBm28 <maleck.abm28@gmail.com>
2025-09-25 12:46:10 +0200

    cambios

---
+++ jardinero.sql
@@ -0,0 +1,10 @@
+create database RetoDin;
+
+use RetoDin;
+
+create table jardinero(nombre varchar(30), apellido varchar(30), email varchar(50), contrasena varchar(30), sueldo float);
+
+insert into jardinero values("pepeman","sani","pepe@gmail.com","pepe69",1500);
+insert into jardinero values("alex","fer","alex@gmail.com","alexxx",1500);
+insert into jardinero values("maleck","benigno","maleck@gmail.com","kelam",1500);
+insert into jardinero values("sachin","sachini","sachin@gmail.com","sachini",1500);
